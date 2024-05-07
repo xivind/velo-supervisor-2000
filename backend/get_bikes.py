3137f7ec2a72eec69b2438afb6838e9620d8d077
Xivind <oivind.langeland@gmail.com>
2024-05-07 06:40:09 +0200

    Pseudo code

--- backend/get_bikes.py
+++ backend/get_bikes.py
@@ -1,4 +1,5 @@
 
 
-
+# Reads from table rides and builde list of gear id, the use this to query Strava API
+# How can you query Sqlite for this without checking the entire tables? 
 # Returns dictionary of data
