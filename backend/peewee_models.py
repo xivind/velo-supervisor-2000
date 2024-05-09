5b92f2cd0f5bfbdc34566f041f7788d8a232acbb
Xivind <oivind.langeland@gmail.com>
2024-05-09 12:02:58 +0200

    Updated meta class with explicit table names

--- backend/peewee_models.py
+++ backend/peewee_models.py
@@ -1,4 +1,5 @@
-"""Configuration and mapping of the SQL database"""
+#!/usr/bin/env python3
+"""Module for configuration and mapping of an SQL database"""
 
 from peewee import SqliteDatabase, Model, CharField, FloatField, IntegerField
 
@@ -6,9 +7,36 @@ database = SqliteDatabase('/home/xivind/SQLiteStudio/Test')  #Move to config
 
 
 class BaseModel(Model):
-    """Base model class for inheritance"""
+    """Base model for inheritance"""
     class Meta:
+        """Extra attributes for base model for inheritance"""
         database = database
+
+
+class Bikes(BaseModel):
+    """Model for table: bikes"""
+    bike_id = CharField(primary_key=True, unique=True)
+    athlete_id = CharField()
+    total_distance = FloatField()
+
+    class Meta:
+        """Extends model with extra attributes"""
+        table_name = "bikes"
+
+
+class Rides(BaseModel):
+    """Model for table: rides"""
+    ride_id = CharField(primary_key=True, unique=True)
+    bike_id = CharField()
+    record_time = CharField()
+    ride_name = CharField()
+    ride_distance = FloatField()
+    moving_time = CharField()
+    commute = CharField()
+
+    class Meta:
+        """Extends model with extra attributes"""
+        table_name = "rides"
 
 
 class Components(BaseModel):
@@ -24,32 +52,27 @@ class Components(BaseModel):
     service_status = CharField()
     updated_date = CharField()
 
+    class Meta:
+        """Extends model with extra attributes"""
+        table_name = "components"
+
 
 class Athletes(BaseModel):
     """Model for table: athletes"""
     ahlete_id = CharField(primary_key=True, unique=True)
 
-
-class Bikes(BaseModel):
-    """Model for table: bikes"""
-    bike_id = CharField(primary_key=True, unique=True)
-    athlete_id = CharField()
-    total_distance = FloatField() 
-
-
-class Rides(BaseModel):
-    """Model for table: rides"""
-    ride_id = CharField(primary_key=True, unique=True)
-    bike_id = CharField()
-    record_time = CharField()
-    ride_name = CharField()
-    ride_distance = FloatField()
-    ride_moving_time = CharField()
+    class Meta:
+        """Extends model with extra attributes"""
+        table_name = "athletes"
 
 
 class Services(BaseModel):
     """Model for table: services"""
     service_id = CharField(primary_key=True, unique=True)
+
+    class Meta:
+        """Extends model with extra attributes"""
+        table_name = "services"
 
 
 __all__ = ['database', 'BaseModel', 'Rides', 'Bikes']
