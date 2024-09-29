0593274b98e3b9e0079bd5e4df2b70ee761db581
Xivind <oivind.langeland@gmail.com>
2024-09-30 01:10:36 +0200

    Script created, but not tested. Review when docker container is deployed. #92

--- backend/backup_db.sh
+++ backend/backup_db.sh
@@ -3,10 +3,10 @@
 
 set -o xtrace
 
-rm -vf /home/pi/backup/nodered.tar
-docker container stop nodered
+rm -vf /home/pi/backup/velo-supervisor-2000.tar
+docker container stop velo-supervisor-2000
 sleep 5
-docker run --rm --volumes-from nodered -v /home/pi/backup:/backup ubuntu tar cvf /backup/nodered.tar /data 
-docker container start nodered
+docker run --rm --volumes-from velo-supervisor-2000 -v /home/pi/backup:/backup ubuntu tar cvf /backup/velo-supervisor-2000.tar /data 
+docker container start velo-supervisor-2000
 
 #For more information, see https://docs.docker.com/storage/volumes/#back-up-restore-or-migrate-data-volumes
