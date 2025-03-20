662b5be4fa36e36579c711ed6aaaf0b62ad7b06f
Øivind Langeland <oivind.langeland@gmail.com>
2025-03-21 00:15:05 +0100

    Updated NIX build

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,24 +1,24 @@
+# To learn more about how to use Nix to configure your environment
+# see: https://developers.google.com/idx/guides/customize-idx-env
 { pkgs, ... }: {
-  channel = "stable-24.05";
-  packages = with pkgs.python311Packages; [
-    jinja2
-    fastapi
-    uvicorn
-    requests_oauthlib
-    peewee
-    python-multipart
-  ] ++ [pkgs.python311];
+  # Which nixpkgs channel to use.
+  channel = "stable-24.05"; # or "unstable"
+  # Use https://search.nixos.org/packages to find packages
+  packages = [
+    pkgs.python3
+    pkgs.python311Full
+  ];
   idx = {
+    # Search for the extensions you want on https://open-vsx.org/ and use "publisher.id"
     extensions = [
       "ms-python.python"
       "ms-python.pylint"
     ];
     workspace = {
+      # Runs when a workspace is first created with this `dev.nix` file
       onCreate = {
-        install = "";
-      };
-      onStart = {
-        shellCommand = "export PYTHONPATH=$(pwd)/backend";
+        install =
+          "python -m venv .venv && source .venv/bin/activate && pip install -r requirements.txt";
       };
     };
   };
